c7188a108e7c1ddd7f451ba83899965b49c9aeb2
Lawrence <loreganlloyd@gmail.com>
2022-10-31 12:53:22 -0400

    create simple add/subtract value buttons to demonstrate useState

--- moochat/App.js
+++ moochat/App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  const clickHandler = () => {
-    console.log("Button pressed! Boop");
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount((previousValue) => {
+      return previousValue + 1;
+    });
+
+    //simplified one line version
+    setCount((previousValue) => previousValue + 1);
+  };
+
+  const subtract = () => {
+    setCount(count - 1);
   };
 
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
-        <Text style={styles.label}>Mooooo!</Text>
-        <StatusBar style="auto" />
+        <Button title="Add" onPress={add} />
+        <Text style={styles.label}>{count}</Text>
 
-        <Button title="First Button" onPress={clickHandler} />
+        <Button title="Subtract" onPress={subtract} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -23,6 +35,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "teal",
+    justifyContent: "center",
+    alignItems: "center",
   },
   label: {
     color: "white",
