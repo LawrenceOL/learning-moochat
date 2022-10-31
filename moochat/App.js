432852cfb460499bb0b0847e6a04e72babd056e6
Lawrence <loreganlloyd@gmail.com>
2022-10-31 12:23:45 -0400

    practice styling test element 'Moooo!'

--- moochat/App.js
+++ moochat/App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.label}>Mooooo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "teal",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label: {
+    color: "white",
+    fontSize: 30,
   },
 });
