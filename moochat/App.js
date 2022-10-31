bc7cb8e2cff51c31e846b4bf2cc365ad2699413b
Lawrence <loreganlloyd@gmail.com>
2022-10-31 17:55:02 -0400

    install expo SplashScreen and setup with loading splash screen with 2 second waiting time

--- moochat/App.js
+++ moochat/App.js
@@ -1,9 +1,32 @@
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Load fonts
+    setTimeout(() => {
+      setAppIsLoaded(true);
+    }, 2000);
+  });
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
       <SafeAreaView>
         <Text style={styles.label}>Welcome to the Beginning</Text>
       </SafeAreaView>
