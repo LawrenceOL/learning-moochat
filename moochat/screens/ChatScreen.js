9367332ad68477e6e8abf3edd317b1b1b316f022
Lawrence <loreganlloyd@gmail.com>
2022-11-01 14:20:12 -0400

    style media and camera buttons

--- moochat/screens/ChatScreen.js
+++ moochat/screens/ChatScreen.js
@@ -21,11 +21,17 @@ const ChatScreen = (props) => {
         style={styles.backgroundImage}
       ></ImageBackground>
       <View style={styles.inputContainer}>
-        <TouchableOpacity onPress={() => console.log("Pressed")}>
+        <TouchableOpacity
+          style={styles.mediaButton}
+          onPress={() => console.log("Pressed")}
+        >
           <Feather name="plus" size={24} color={colors.blue} />
         </TouchableOpacity>
-        <TextInput />
-        <TouchableOpacity onPress={() => console.log("Pressed")}>
+        <TextInput style={styles.textBox} />
+        <TouchableOpacity
+          style={styles.mediaButton}
+          onPress={() => console.log("Pressed")}
+        >
           <Feather name="camera" size={24} color={colors.blue} />
         </TouchableOpacity>
       </View>
@@ -46,6 +52,19 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     paddingHorizontal: 10,
     height: 50,
+  },
+  textBox: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: colors.lightGrey,
+    marginHorizontal: 15,
+    paddingHorizontal: 12,
+  },
+  mediaButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 35,
   },
 });
 
