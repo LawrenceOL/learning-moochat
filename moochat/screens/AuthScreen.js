ced0ea7b0c01054a8001f6ebb292c2d7cdcb69c5
Lawrence <loreganlloyd@gmail.com>
2022-11-01 18:03:21 -0400

    add image to sign up and sign in form

--- moochat/screens/AuthScreen.js
+++ moochat/screens/AuthScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Feather, FontAwesome } from "@expo/vector-icons";
 
@@ -8,6 +8,8 @@ import PageContainer from "../components/PageContainer";
 import SubmitButton from "../components/SubmitButton";
 import SignUpForm from "../components/SignUpForm";
 import SignInForm from "../components/SignInForm";
+import colors from "../constants/colors";
+import logo from "../assets/images/logo.png";
 
 const AuthScreen = (props) => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -15,18 +17,44 @@ const AuthScreen = (props) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <PageContainer>
+        <View style={styles.imageContainer}>
+          <Image source={logo} style={styles.image} />
+        </View>
+
         {isSignUp ? <SignUpForm /> : <SignInForm />}
 
         <TouchableOpacity
           onPress={() => setIsSignUp((prevState) => !prevState)}
+          style={styles.linkContainer}
         >
-          <Text>{`Switch to ${isSignUp ? "sign in" : "sign up"}`}</Text>
+          <Text style={styles.link}>{`Switch to ${
+            isSignUp ? "sign in" : "sign up"
+          }`}</Text>
         </TouchableOpacity>
       </PageContainer>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  linkContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 15,
+  },
+  link: {
+    color: colors.blue,
+    fontFamily: "medium",
+    letterSpacing: 0.3,
+  },
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "60%",
+    resizeMode: "contain",
+  },
+});
 
 export default AuthScreen;
