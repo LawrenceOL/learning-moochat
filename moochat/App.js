31699d7c596febc7b00439ad4174246cbc0a038b
Lawrence <loreganlloyd@gmail.com>
2022-11-01 11:32:04 -0400

    style ChatListScreen

--- moochat/App.js
+++ moochat/App.js
@@ -54,13 +54,11 @@ export default function App() {
 
   return (
     <SafeAreaProvider style={styles.container} onLayout={onLayout}>
-      <SafeAreaView>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={ChatListScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={ChatListScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
@@ -69,8 +67,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center",
   },
   label: {
     color: "black",
--- moochat/Components/ChatListScreen.js
+++ moochat/Components/ChatListScreen.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const ChatListScreen = (props) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Chat list component</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default ChatListScreen;
