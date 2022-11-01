ce7a5565a5d6e70d52b5b877b40e8b844a5e5ab7
Lawrence <loreganlloyd@gmail.com>
2022-11-01 15:21:05 -0400

    ensure PageContainer uses styles from children components by passing props.children. Spread operator on two styles combines them, overiding parent styles with children

--- moochat/screens/AuthScreen.js
+++ moochat/screens/AuthScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import PageContainer from "../components/PageContainer";
 
 const AuthScreen = (props) => {
   return (
-    <SafeAreaView>
-      <Text>Auth Screen</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <PageContainer>
+        <Text>Hello</Text>
+      </PageContainer>
     </SafeAreaView>
   );
 };
