2f47e34009120ce15fe4a76bd58efd9beeb4c73d
Lawrence <loreganlloyd@gmail.com>
2022-12-19 12:34:51 -0500

    create common styles sheet and use on StartUpScreen

--- moochat/navigation/AppNavigator.js
+++ moochat/navigation/AppNavigator.js
@@ -6,17 +6,21 @@ import MainNavigator from "./MainNavigator";
 import ChatSettingsScreen from "../screens/ChatSettingsScreen";
 import AuthScreen from "../screens/AuthScreen";
 import { useSelector } from "react-redux";
+import StartUpScreen from "../screens/StartUpScreen";
 
 const AppNavigator = (props) => {
   const isAuth = useSelector(
     (state) => state.auth.token !== null && state.auth.token !== ""
   );
 
+  const didTryAutoLogin = useSelector((state) => state.auth.didTryAutoLogin);
+
   return (
     // will contain multiple navigators rendered conditionally
     <NavigationContainer>
       {isAuth && <MainNavigator />}
-      {!isAuth && <AuthScreen />}
+      {!isAuth && didTryAutoLogin && <AuthScreen />}
+      {!isAuth && !didTryAutoLogin && <StartUpScreen />}
     </NavigationContainer>
   );
 };
--- moochat/screens/StartUpScreen.js
+++ moochat/screens/StartUpScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { ActivityIndicator, View } from "react-native";
 import colors from "../constants/colors";
+import commonStyles from "../constants/commonStyles";
 
 const StartUpScreen = () => {
   return (
-    <View>
+    <View style={commonStyles.center}>
       <ActivityIndicator size="large" color={colors.primary} />
     </View>
   );
