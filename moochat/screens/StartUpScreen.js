5cd42850f63182804cc58d2d8e08b182668d8b78
Lawrence <loreganlloyd@gmail.com>
2022-12-20 09:37:01 -0500

    log in persists using async storage

--- moochat/screens/StartUpScreen.js
+++ moochat/screens/StartUpScreen.js
@@ -4,7 +4,8 @@ import { ActivityIndicator, View } from "react-native";
 import { useDispatch } from "react-redux";
 import colors from "../constants/colors";
 import commonStyles from "../constants/commonStyles";
-import { setDidTryAutoLogin } from "../store/authSlice";
+import { authenticate, setDidTryAutoLogin } from "../store/authSlice";
+import { getUserData } from "../utils/actions/userActions";
 
 const StartUpScreen = () => {
   const dispatch = useDispatch();
@@ -18,6 +19,19 @@ const StartUpScreen = () => {
         dispatch(setDidTryAutoLogin());
         return;
       }
+
+      const parseData = JSON.parse(storedAuthInfo);
+      const { token, userId, expiryDate: expiraryDateString } = parseData;
+
+      const expiryDate = new Date(expiraryDateString);
+
+      if (expiryDate <= new Date() || !token || !userId) {
+        dispatch(setDidTryAutoLogin());
+        return;
+      }
+
+      const userData = await getUserData(userId);
+      dispatch(authenticate({ token: token, userData }));
     };
     tryLogin();
   }, []);
